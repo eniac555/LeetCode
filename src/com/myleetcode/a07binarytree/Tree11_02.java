84f2f38cbf06c4b56bbb3b99c9e2a11cb52e52a9
eniac555 <1150304664@qq.com>
2023-04-17 22:25:59 +0800

    二叉树

---
+++ src/com/myleetcode/a07binarytree/Tree11_02.java
@@ -0,0 +1,68 @@
+package com.myleetcode.a07binarytree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 判断平衡二叉树
+ */
+public class Tree11_02 {
+    /**
+     * 迭代法，效率较低，计算高度时会重复遍历
+     * 时间复杂度：O(n^2)
+     */
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode inNode = stack.peek();
+            // 右结点为null或已经遍历过
+            if (inNode.right == null || inNode.right == pre) {
+                // 比较左右子树的高度差，输出
+                if (Math.abs(getHeight(inNode.left) - getHeight(inNode.right)) > 1) {
+                    return false;
+                }
+                stack.pop();
+                pre = inNode;
+                root = null;// 当前结点下，没有要遍历的结点了
+            } else {
+                root = inNode.right;// 右结点还没遍历，遍历右结点
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 层序遍历，求结点的高度
+     */
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+        int depth = 0;
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = deque.poll();
+                if (poll.left != null) {
+                    deque.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    deque.offer(poll.right);
+                }
+            }
+        }
+        return depth;
+    }
+}
