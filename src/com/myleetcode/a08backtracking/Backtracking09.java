064d3d856019926f3e5a4253771fb990bc52c6f4
eniac555 <1150304664@qq.com>
2023-05-18 20:27:02 +0800

    回溯

---
+++ src/com/myleetcode/a08backtracking/Backtracking09.java
@@ -0,0 +1,36 @@
+package com.myleetcode.a08backtracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author eniac555
+ * @date 2023/5/18
+ * @description: 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * <p>
+ * 说明：解集不能包含重复的子集。
+ * <p>
+ * 示例: 输入: nums = [1,2,3] 输出: [ [3],   [1],   [2],   [1,2,3],   [1,3],   [2,3],   [1,2],   [] ]
+ */
+public class Backtracking09 {
+    List<List<Integer>> result = new ArrayList<>();// 存放符合条件结果的集合
+    LinkedList<Integer> path = new LinkedList<>();// 用来存放符合条件结果
+
+    public List<List<Integer>> subsets(int[] nums) {
+        subsetsHelper(nums, 0);
+        return result;
+    }
+
+    private void subsetsHelper(int[] nums, int startIndex) {
+        result.add(new ArrayList<>(path));//「遍历这个树的时候，把所有节点都记录下来，就是要求的子集集合」。
+        if (startIndex >= nums.length) { //终止条件可不加
+            return;
+        }
+        for (int i = startIndex; i < nums.length; i++) {
+            path.add(nums[i]);
+            subsetsHelper(nums, i + 1);
+            path.removeLast();
+        }
+    }
+}
