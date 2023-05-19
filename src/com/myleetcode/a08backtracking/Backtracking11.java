978313144cfa465f73fb92cf4c018ed8bd57d19a
eniac555 <1150304664@qq.com>
2023-05-19 20:48:42 +0800

    回溯

---
+++ src/com/myleetcode/a08backtracking/Backtracking11.java
@@ -0,0 +1,46 @@
+package com.myleetcode.a08backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author eniac555
+ * @date 2023/5/19
+ * @description: 给定一个整型数组, 你的任务是找到所有该数组的递增子序列，递增子序列的长度至少是2。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [4, 6, 7, 7]
+ * 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+ * 说明:
+ * <p>
+ * 给定数组的长度不会超过15。
+ * 数组中的整数范围是 [-100,100]。
+ * 给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
+ */
+public class Backtracking11 {
+
+    private List<Integer> path = new ArrayList<>();
+    private List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        backtracking(nums, 0);
+        return res;
+    }
+
+    private void backtracking(int[] nums, int start) {
+        if (path.size() > 1) {
+            res.add(new ArrayList<>(path));
+        }
+
+        int[] used = new int[201];
+        for (int i = start; i < nums.length; i++) {
+            if (!path.isEmpty() && nums[i] < path.get(path.size() - 1) ||
+                    (used[nums[i] + 100] == 1)) continue;
+            used[nums[i] + 100] = 1;
+            path.add(nums[i]);
+            backtracking(nums, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
---
+++ src/com/myleetcode/a08backtracking/Backtracking12.java
@@ -0,0 +1,9 @@
+package com.myleetcode.a08backtracking;
+
+/**
+ * @author eniac555
+ * @date 2023/5/19
+ * @description:
+ */
+public class Backtracking12 {
+}
