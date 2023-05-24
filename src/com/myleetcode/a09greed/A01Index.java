2718a942720890d6c753d902694edfbaa08336cf
eniac555 <1150304664@qq.com>
2023-05-24 20:00:53 +0800

    贪婪算法

---
+++ src/com/myleetcode/a09greed/A01Index.java
@@ -0,0 +1,16 @@
+package com.myleetcode.a09greed;
+
+/**
+ * @author eniac555
+ * @date 2023/5/24
+ * @description:
+ */
+public class A01Index {
+
+    /*
+
+    01: 分发饼干
+    02: 摆动序列
+
+     */
+}
---
+++ src/com/myleetcode/a09greed/Greed01.java
@@ -0,0 +1,56 @@
+package com.myleetcode.a09greed;
+
+import java.util.Arrays;
+
+/**
+ * @author eniac555
+ * @date 2023/5/24
+ * @description: 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ * <p>
+ * 对每个孩子 i，都有一个胃口值  g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+ * <p>
+ * 示例  1:
+ * <p>
+ * 输入: g = [1,2,3], s = [1,1]
+ * 输出: 1 解释:你有三个孩子和两块小饼干，3 个孩子的胃口值分别是：1,2,3。虽然你有两块小饼干，由于他们的尺寸都是 1，你只能让胃口值是 1 的孩子满足。所以你应该输出 1。
+ * 示例  2:
+ * <p>
+ * 输入: g = [1,2], s = [1,2,3]
+ * 输出: 2
+ * 解释:你有两个孩子和三块小饼干，2 个孩子的胃口值分别是 1,2。你拥有的饼干数量和尺寸都足以让所有孩子满足。所以你应该输出 2.
+ */
+public class Greed01 {
+    // 思路: 优先考虑饼干，小饼干先喂饱小胃口
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int start = 0;
+        int count = 0;
+        for (int i = 0; i < s.length && start < g.length; i++) {
+            if (s[i] >= g[start]) {
+                start++;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // 思路：优先考虑胃口，先喂饱大胃口
+    public int findContentChildren2(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        int start = s.length - 1;
+        // 遍历胃口
+        for (int index = g.length - 1; index >= 0; index--) {
+            if (start >= 0 && g[index] <= s[start]) {
+                start--;
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+}
