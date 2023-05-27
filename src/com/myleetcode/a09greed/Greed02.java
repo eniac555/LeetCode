07a3889023121beebe8eb15eb30a27d6405a132c
eniac555 <1150304664@qq.com>
2023-05-27 19:49:02 +0800

    贪心算法

--- src/com/myleetcode/a09greed/Greed02.java
+++ src/com/myleetcode/a09greed/Greed02.java
@@ -49,5 +49,5 @@ public class Greed02 {
         return count;
     }
 
-    
+
 }
---
+++ src/com/myleetcode/a09greed/Greed03.java
@@ -0,0 +1,31 @@
+package com.myleetcode.a09greed;
+
+/**
+ * @author eniac555
+ * @date 2023/5/27
+ * @description: 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释:  连续子数组  [4,-1,2,1] 的和最大，为  6。
+ */
+public class Greed03 {
+
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int sum = Integer.MIN_VALUE;
+        int count = 0;
+        for (int num : nums) {
+            count += num;
+            sum = Math.max(sum, count); // 取区间累计的最大值（相当于不断确定最大子序终止位置）
+            if (count <= 0) {
+                count = 0; // 相当于重置最大子序起始位置，因为遇到负数一定是拉低总和
+            }
+        }
+        return sum;
+    }
+}
