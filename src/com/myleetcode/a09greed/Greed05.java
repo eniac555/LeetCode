6c66dac68d907afddd2c21c42aea4377c0a97633
eniac555 <1150304664@qq.com>
2023-05-28 21:32:25 +0800

    贪心算法

---
+++ src/com/myleetcode/a09greed/Greed05.java
@@ -0,0 +1,40 @@
+package com.myleetcode.a09greed;
+
+/**
+ * @author eniac555
+ * @date 2023/5/28
+ * @description: 给定一个非负整数数组，你最初位于数组的第一个位置。
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 判断你是否能够到达最后一个位置。
+ * <p>
+ * 示例  1:
+ * <p>
+ * 输入: [2,3,1,1,4]
+ * 输出: true
+ * 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+ * <p>
+ * 示例  2:
+ * <p>
+ * 输入: [3,2,1,0,4]
+ * 输出: false
+ * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置
+ */
+public class Greed05 {
+
+    public boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        //覆盖范围, 初始覆盖范围应该是0，因为下面的迭代是从下标0开始的
+        int coverRange = 0;
+        //在覆盖范围内更新最大的覆盖范围
+        for (int i = 0; i <= coverRange; i++) {
+            coverRange = Math.max(coverRange, i + nums[i]);
+            if (coverRange >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
